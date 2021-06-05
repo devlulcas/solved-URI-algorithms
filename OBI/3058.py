0131d3589abdd6bea9b227a72559408768fe7792
devlulcas <lucasalvesrego.contato@gmail.com>
2021-06-05 00:22:06 -0300

    I SUCK AT CODING AND IM SAD NOW

--- OBI/3058.py
+++ OBI/3058.py
@@ -3,6 +3,6 @@ n = int(input())
 precos = []
 for i in range(0, n):
   p, g = [float(num) for num in input().split(" ")]
-  ppg = p / g 
-  precos.append(ppg * 1000)
-print(min(precos))
+  pf = (p / g) * 1000
+  precos.append(pf)
+print(f"{min(precos):.2f}")
