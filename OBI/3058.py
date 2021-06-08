0c75fcfcaf98ae9bd0fcadfb4a6d9c65532646d6
devlulcas <lucasalvesrego.contato@gmail.com>
2021-06-07 23:37:15 -0300

    fix: typo

--- OBI/3058.py
+++ OBI/3058.py
@@ -1,8 +1,7 @@
 #URI 3058 - SUPERMERCADO
 n = int(input())
 precos = []
-for i in range(0, n):
+for i in range(n):
   p, g = [float(num) for num in input().split(" ")]
-  pf = (p / g) * 1000
-  precos.append(pf)
+  precos.append((p / g) * 1000)
 print(f"{min(precos):.2f}") 
